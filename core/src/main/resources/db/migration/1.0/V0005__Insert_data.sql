70e5ab1b44cfeee6a0f3b70bc38f75b93123d4ed
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-06-19 10:49:15 +0530

    added mock data

--- core/src/main/resources/db/migration/1.0/V0005__Insert_data.sql
+++ core/src/main/resources/db/migration/1.0/V0005__Insert_data.sql
@@ -4,22 +4,23 @@ INSERT INTO MUTITAT (id, modificationCounter, TipodeTarifa, description) VALUES 
 INSERT INTO MUTITAT (id, modificationCounter, TipodeTarifa, description) VALUES (3, 1, 'C', 'sampleC');
 INSERT INTO MUTITAT (id, modificationCounter, TipodeTarifa, description) VALUES (4, 1, 'D', 'sampleD');
 INSERT INTO MUTITAT (id, modificationCounter, TipodeTarifa, description) VALUES (5, 1, 'E', 'sampleE');
+INSERT INTO MUTITAT (id, modificationCounter, TipodeTarifa, description) VALUES (6, 1, 'F', 'sampleF');
 
 --MUPGPT
 INSERT INTO MUPGPT (id, modificationCounter, tipodeTarifa, fechaDesdeVigencia, importeParkingMax, importeMinSinCompra, tiempoMaxSinCompra, importeMin1Hora,
-importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (1, 1, 1, 20190606, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (1, 1, 1, 20190412, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
 INSERT INTO MUPGPT (id, modificationCounter, tipodeTarifa, fechaDesdeVigencia, importeParkingMax, importeMinSinCompra, tiempoMaxSinCompra, importeMin1Hora,
-importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (2, 1, 1, 20190607, 2, 2, 2, 2, 2, 2, 2, 2, 2);
+importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (2, 1, 2, 20180415, 2, 2, 2, 2, 2, 2, 2, 2, 2);
 
 INSERT INTO MUPGPT (id, modificationCounter, tipodeTarifa, fechaDesdeVigencia, importeParkingMax, importeMinSinCompra, tiempoMaxSinCompra, importeMin1Hora,
-importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (3, 1, 1, 20190608, 3, 3, 3, 3, 3, 3, 3, 3, 3);
+importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (3, 1, 5, 20190608, 3, 3, 3, 3, 3, 3, 3, 3, 3);
 
 INSERT INTO MUPGPT (id, modificationCounter, tipodeTarifa, fechaDesdeVigencia, importeParkingMax, importeMinSinCompra, tiempoMaxSinCompra, importeMin1Hora,
-importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (4, 1, 1, 20190609, 4, 4, 4, 4, 4, 4, 4, 4, 4);
+importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (4, 1, 2, 20190609, 4, 4, 4, 4, 4, 4, 4, 4, 4);
 
 INSERT INTO MUPGPT (id, modificationCounter, tipodeTarifa, fechaDesdeVigencia, importeParkingMax, importeMinSinCompra, tiempoMaxSinCompra, importeMin1Hora,
-importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (5, 1, 1, 20190610, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+importeMin2Hora, fraccionFacturacion, costeFraccion, tiempoMaxSalida, fechaModificacion) VALUES (5, 1, 1, 20190412, 5, 5, 5, 5, 5, 5, 5, 5, 5);
 
 
 --CPTDAST maestro
@@ -33,4 +34,5 @@ INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVi
 INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVigencia, fechaModificacion) VALUES (2, 1, 1, 3, 20140415, 20140512 );
 INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVigencia, fechaModificacion) VALUES (3, 1, 3, 2, 20180415, 20180512 );
 INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVigencia, fechaModificacion) VALUES (4, 1, 2, 1, 20150415, 20150512 );
-
+INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVigencia, fechaModificacion) VALUES (5, 1, 4, 2, 20150515, 20150612 );
+INSERT INTO MUCETAT (id, modificationCounter, centro, tipodeTarifa, fechaDesdeVigencia, fechaModificacion) VALUES (6, 1, 4, 1, 20150417, 20150712 );
